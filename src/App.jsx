775285a2cea3ec4cb64907b7c19801b02391fc62
dbarrera <dbarrerapalacios@gmail.com>
2020-12-22 11:55:07 -0500

    se cambio la estructura

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Contador from './components/Contador';
+
+function App() {
+  return (
+<div>
+  <Contador></Contador>
+</div>
+  );
+}
+
+export default App;
---
+++ src/components/Contador.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contador() {
+    return (
+        <div>
+            Hola
+        </div>
+    )
+}
+
+export default Contador
